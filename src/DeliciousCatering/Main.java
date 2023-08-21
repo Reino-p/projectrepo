d3c51ee10d456e80212c93edd1699b94d480e252
Reino-p <reinopowell61@gmail.com>
2023-08-21 19:35:26 +0200

    Full Login Functionality

--- src/DeliciousCatering/Main.java
+++ src/DeliciousCatering/Main.java
@@ -3,6 +3,7 @@ package DeliciousCatering;
 
 public class Main {
     public static void main(String[] args) {
+        //This is where we start the program
         LoginPage lp = new LoginPage();
         lp.setVisible(true);
         
