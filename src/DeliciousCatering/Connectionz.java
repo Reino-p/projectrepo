d406d33eb30a9b53e9494ef1b0107873bccf34d8
Reino-p <reinopowell61@gmail.com>
2023-08-21 18:54:54 +0200

    Project

---
+++ src/DeliciousCatering/Connectionz.java
@@ -0,0 +1,20 @@
+package DeliciousCatering;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connectionz {
+    static Connection con;
+    
+    public static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
+                                                                        //database name, "db username", "Password"
+        }catch(Exception ex){
+            System.out.println(""+ex);
+        }
+        return con;
+    }
+    
+}
---
+++ src/DeliciousCatering/Main.java
@@ -0,0 +1,10 @@
+
+package DeliciousCatering;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginPage lp = new LoginPage();
+        lp.setVisible(true);
+        
+    }
+}
